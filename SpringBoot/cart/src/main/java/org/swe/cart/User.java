fe5ffe4a4f19dae10abe6b520fc7dbbfac101231
Jonah200 <jonahlorenzo2004@gmail.com>
2025-02-20 14:24:58 -0500

    Get Spring mysql connection working

--- SpringBoot/cart/src/main/java/org/swe/cart/User.java
+++ SpringBoot/cart/src/main/java/org/swe/cart/User.java
@@ -11,7 +11,7 @@ import jakarta.persistence.Table;
 public class User {
 
 	@Id
-	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@GeneratedValue(strategy=GenerationType.AUTO)
 	private Integer id;
 
 	private String name;
--- SpringBoot/cart/src/main/java/org/swe/cart/UserRepository.java
+++ SpringBoot/cart/src/main/java/org/swe/cart/UserRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.repository.CrudRepository;
 import org.swe.cart.User;
 
 //Spring will automatically generate this file
+
 public interface UserRepository extends CrudRepository<User, Integer> {
     
 }
