686bd996baabdd2f706b8c3f2d2e48d47c84df27
Jonah200 <jonahlorenzo2004@gmail.com>
2025-02-23 22:23:40 -0500

    Added Forgot Password Screen

--- share_cart_flutter/lib/login_screen.dart
+++ share_cart_flutter/lib/login_screen.dart
@@ -17,10 +17,9 @@ class _LoginPageState extends State<LoginPage> {
     final theme = Theme.of(context);
 
     return Scaffold(
-      body: Padding(
-        //TODO try and add shopping cart image
-        padding: const EdgeInsets.all(10),
+      body: Center(
         child: ListView(
+          shrinkWrap: true,
           children: <Widget>[
             Container(
               alignment: Alignment.center,
@@ -65,9 +64,9 @@ class _LoginPageState extends State<LoginPage> {
               TextButton(
                 onPressed: () {
                   //TODO: Implement Forgot Password Screen
-                  Navigator.pushReplacement(
+                  Navigator.push(
                     context,
-                    MaterialPageRoute(builder: (context) => const Placeholder()));
+                    MaterialPageRoute(builder: (context) => const ForgotPasswordPage()));
                 },
                 child: const Text("Forgot Password"),
               ),
@@ -109,12 +108,80 @@ class _LoginPageState extends State<LoginPage> {
                 
               )
           ],
-        ),),
+        ),
+      ),
     );
 
   }
 }
 
-// class ForgotPasswordScreen extends StatefulWidget{
+class ForgotPasswordPage extends StatefulWidget{
+  const ForgotPasswordPage({super.key});
 
-// }
+  @override
+  State<ForgotPasswordPage> createState() => _ForgotPasswordPage();
+}
+
+class _ForgotPasswordPage extends State<ForgotPasswordPage> {
+  TextEditingController emailController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+
+    return Scaffold(
+      
+      body: Center(
+        child: ListView(
+          shrinkWrap: true,
+          children: <Widget>[
+            Container(
+              alignment: Alignment.center,
+              padding: const EdgeInsets.all(10),
+              child: Text(
+                'Forgot Password?',
+                style: TextStyle(
+                  color: theme.primaryColor,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 30
+                ),
+              ),
+            ),
+            Container(
+                padding: const EdgeInsets.all(10),
+                child: TextField(
+                  controller: emailController,
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Email'
+                  ),
+                ),
+              ),
+            Container(
+              height: 50,
+              padding: const EdgeInsets.all(10),
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.zero
+                  ),
+                  backgroundColor: theme.primaryColor,
+                  foregroundColor: theme.colorScheme.onPrimary,
+                ),
+                onPressed: () {
+                  //TODO Implement forgot password system, show something to indicate that button was pressed
+                },
+                child: const Text('Send Email')), //TODO: Possibly change to 'Resend Email' after pressed.
+              ),
+              TextButton(
+              onPressed: () {
+                //TODO Verify this is the correct action
+                Navigator.pop(context);
+              },
+              child: const Text('Return to Sign In'))
+          ],
+        ),
+        ),
+    );
+  }
+}
